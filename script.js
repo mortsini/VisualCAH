8885d0ea69ca23c3e6eba6dcb36f9d8f5161c32f
xiao-git <mortsini@gmail.com>
2014-09-15 01:55:59 -0700

    Load strings from data

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+var questions;
+var answers;
+
 $(document).ready(function(){
 	$('p').css("background-color","#0000FF");
+	$.get("against-humanity/questions.txt", function(data,status){
+		questions = data.split('\n');
+	}, 'text');
+	$.get("against-humanity/answers.txt", function(data,status){
+		answers = data.split('\n');
+	}, 'text');
+	
 	$('#action_deal').click(function(){
-		var questionsFile = "./against-humanity/questions.txt";
-		var answersFile = "./against-humanity/answers.txt";
 		
 		
 		
-		
-		$('#cards').append('<div class="card white-card">moo</div>');
+		$('#cards').append('<div class="card white-card">'+answers[9]+'</div>');
 	});
 });
