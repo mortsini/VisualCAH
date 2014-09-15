32894247150392fcb42880821334ab66fd320f34
xiao-git <mortsini@gmail.com>
2014-09-15 00:05:39 -0700

    Spawn cards view

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('p').css("background-color","#0000FF");
+	$('#action_deal').click(function(){
+		var questionsFile = "./against-humanity/questions.txt";
+		var answersFile = "./against-humanity/answers.txt";
+		
+		
+		
+		
+		$('#cards').append('<div class="card white-card">moo</div>');
+	});
+});
