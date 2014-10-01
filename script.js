ea234889188c55514a40458cec993ecd6802b71a
xiao-git <mortsini@gmail.com>
2014-09-30 22:25:56 -0700

    Put footer on bottom

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var questions;
 var answers;
-
+var dealing = false;
 $(document).ready(function(){
 	$.get("against-humanity/questions.txt", function(data,status){
 		questions = data.split('\n');
@@ -9,21 +9,30 @@ $(document).ready(function(){
 		answers = data.split('\n');
 	}, 'text');
 	
-	$('#action_deal').click(dealCards);
-	$(document).keypress(dealCards);
+	$('html').click(clearAndDealCards);
+	$(document).keypress(clearAndDealCards);
 });
 
+function clearAndDealCards() {
+	if (!dealing) {
+		dealing = true;
+		$('#card-content').slideUp(function(){
+			$('.card-container').empty();
+			$('#card-content').slideDown(dealCards);
+		});
+	}
+}
 function dealCards() {
 	var questionString = questions[Math.floor(questions.length * Math.random())];
 		var numAnswers = Math.max(1, (questionString.match(/_+[^_]*/g) || []).length);
-		$('#questions-container').empty();
-		$('#answers-container').empty();
 		$('#questions-container').append(toCard(questionString, false));
 		for (var i = 0; i < numAnswers; i++) {
 			var answerString = answers[Math.floor(answers.length * Math.random())];
 			$('#answers-container').append(toCard(answerString, true));
 		}
-		$('.card').effect('slide');
+		$('.card').effect('slide', function(){
+			dealing = false;
+		});
 }
 function toCard(text, isAnswer) {
 	var colorType = (isAnswer) ? 'a' : 'q';
